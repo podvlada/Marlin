a06265fbe3389baceda4e78d5dc1eb432af69004
vlad <vlada.pod@gmail.com>
2023-08-02 16:58:21 +0200

    serial_2 changed to 3, for use with ESP3D wifi

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -109,7 +109,7 @@
  * Currently only supported for AVR, DUE, LPC1768/9 and STM32/STM32F1
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_3 2
+#define SERIAL_PORT_3 3
 //#define BAUDRATE_3 250000   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 // Enable the Bluetooth serial interface on AT90USB devices
