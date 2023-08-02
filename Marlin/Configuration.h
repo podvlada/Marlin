010444217b7d6188ba7c664398d3070affca39ae
vlad <vlada.pod@gmail.com>
2023-08-02 18:22:36 +0200

    sd card no usb

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -116,7 +116,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MP3DP v.05"
+#define CUSTOM_MACHINE_NAME "MP3DP v.06"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
